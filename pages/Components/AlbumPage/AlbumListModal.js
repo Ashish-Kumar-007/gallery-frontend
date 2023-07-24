bf34914e9f49b14d8d5dcf61385e6af346f2b117
Ash45# <ashishsahoo2015@gmail.com>
2023-07-25 01:05:46 +0530

    updated code r-3

--- pages/Components/AlbumPage/AlbumListModal.js
+++ pages/Components/AlbumPage/AlbumListModal.js
@@ -7,8 +7,8 @@ const AlbumListModal = ({ album, button, imageId }) => {
   return (
 <div className="flex flex-row border justify-center border-gray-300 rounded p-4 shadow-md items-center">
   <div className="flex flex-col mr-2">
-    <Image src={album.album_image} alt={album.title} height={100} width={120} />
-    <h3 className="text-xl font-semibold">{title}</h3>
+    <Image src={album?.album_image} alt={album?.title} height={100} width={120} />
+    <h3 className="text-xl font-semibold">{album?.title}</h3>
   </div>
   {button}
 </div>
